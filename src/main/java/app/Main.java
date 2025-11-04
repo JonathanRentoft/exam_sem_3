7a5419f2b65a76eab74ae9964e99b120ab551612
Jonathan0912000 <Jonathan0912@gmail.com>
2025-11-04 11:05:37 +0100

    databaseopsætning

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -7,11 +7,6 @@ public class Main {
     
     public static void main(String[] args) {
         Javalin app = ApplicationConfig.startServer();
-        
-        // Graceful shutdown hook
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            System.out.println("Shutting down Trip Planning API");
-            ApplicationConfig.stopServer(app);
-        }));
+
     }
 }
